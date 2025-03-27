801315d42de2f346997161467bd3858f4291b435
Marcio Rogerio Nizzola <marcio@nizzola.com.br>
2025-03-27 20:37:51 -0300

    add css to components

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MyComponent from "./components/MyComponent";
+import MyComponent2 from "./components/MyComponent2";
 import './App.css'
 
 function App() {
@@ -8,9 +8,14 @@ function App() {
 
   return (
     <>
-      <h1>
-        Hoje tem final de campeonato
-      </h1>
+      <div>
+        <h1>Aula</h1>
+        <MyComponent />
+        <MyComponent2 title="Titulo personalizado 1" description="Tipo 1." />
+        <MyComponent2 title="Titulo personalizado 2" description="Tipo 2." />
+        <MyComponent2 title="ESCOLA NAZARENA" description="Tipo 2." />
+        
+      </div>
     </>
   )
 }
---
+++ meu-projeto/src/components/MyComponent.jsx
@@ -0,0 +1,10 @@
+import "./MyComponent.css";
+
+export default function MyComponent() {
+    return (
+      <div class="my-component-container">
+        <h1 class="my-component-title">Primeiro Componente</h1>
+        <p class="my-component-description">Aqui é o meu componente 1</p>
+      </div>
+    );
+  }
